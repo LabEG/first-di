3a264e2551095d06122855dab6a4feb3820ddf5c
Eugene Labutin (CI/CD) <labeg@mail.ru>
2022-11-21 14:17:39 +0000

    chore: bump dependencies

--- dist/typings/class-constructor.d.ts
+++ dist/typings/class-constructor.d.ts
@@ -1,4 +1,4 @@
-export declare type ClassConstructor<T extends object> = (new (...params: any) => T);
-export declare type OverrideConstructor<T extends object> = ClassConstructor<T> | (Function & {
+export type ClassConstructor<T extends object> = (new (...params: any) => T);
+export type OverrideConstructor<T extends object> = ClassConstructor<T> | (Function & {
     prototype: T;
 });
